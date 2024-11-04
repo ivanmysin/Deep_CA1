3a398a5260ddf070d9c807dc33736bb41ae4cc8a
ivan <imysin@mail.ru>
2024-11-04 13:37:38 +0300

    Nan in gradients are debbuged

---
+++ tests/test_cpu_conv.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import numpy as np
+from keras.src.ops import dtype
+from tensorflow import GradientTape
+
+
+x = tf.Variable(np.ones(3), dtype=tf.complex64)
+kernel = tf.constant( np.ones(3), dtype=tf.complex64)
+
+
+with GradientTape() as tape:
+    Nfull = tf.size(kernel) + tf.size(x) - 1
+
+
+
+    K = tf.signal.fft(kernel)
+    X = tf.signal.fft(x)
+
+    C = tf.signal.ifft(K * X)
+
+    C = tf.math.real(C)
+
+    Cs = tf.reduce_mean(C)
+
+grad = tf.cast( tape.gradient(Cs, x), dtype=tf.float32)
+
+print(grad)
