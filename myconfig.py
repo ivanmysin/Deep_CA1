c6523433e49e95504b9c4d116cea5f8801fd5f57
ivan <imysin@mail.ru>
2024-12-23 23:14:42 +0300

    Working on pop models fitting

--- myconfig.py
+++ myconfig.py
@@ -33,7 +33,7 @@ DURATION = 2000 # ms
 
 ##############################################
 ######### fit models of populations ##########
-NEPOCHES = 1200
+NEPOCHES = 400
 TRAIN2TESTRATIO = 0.9
 VERBOSETRANINGPOPMODELS = 0
 BATCHSIZE = 200
