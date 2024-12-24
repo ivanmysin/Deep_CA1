f7a62b94d04f4056b5e337806679e13ac41f2346
ivan <imysin@mail.ru>
2024-12-24 18:28:30 +0300

    Work on pop models fitting

--- dl_deep_pop_model.py
+++ dl_deep_pop_model.py
@@ -161,7 +161,11 @@ def fit_dl_model_of_population(datapath, targetpath, logfile):
         model.add( Dense(16, activation='sigmoid') )  #
         model.add( LSTM(16, return_sequences=True, kernel_initializer=keras.initializers.HeUniform(), stateful=False ) ) # , stateful=True
         model.add( LSTM(16, return_sequences=True, kernel_initializer=keras.initializers.HeUniform(), stateful=False ) ) # , stateful=True
-        model.add( Dense(1, activation='relu') ) #
+        #model.add( Dense(1, activation='relu') ) #
+        model.add( Dense(units = 1,
+                         kernel_initializer = 'random_normal',
+                         bias_initializer = 'ones',
+                         activation = 'relu') ) #
 
         # model.add( GRU(16, return_sequences=True, kernel_initializer=keras.initializers.HeUniform(), stateful=True ) ) #, stateful=True
         # model.add( GRU(16, return_sequences=True, kernel_initializer=keras.initializers.HeUniform(), stateful=True ) ) # , stateful=True
