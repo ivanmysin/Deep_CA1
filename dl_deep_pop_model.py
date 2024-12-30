536fa370c7af690e132508bb0883a92336a02558
ivan <imysin@mail.ru>
2024-12-30 19:01:44 +0300

    Work on big model running

--- dl_deep_pop_model.py
+++ dl_deep_pop_model.py
@@ -160,11 +160,13 @@ def fit_dl_model_of_population(datapath, targetpath, logfile):
         model.add( Input(shape=(None, 1)) )
         model.add( LSTM(32, return_sequences=True, kernel_initializer=keras.initializers.Zeros(), \
                         stateful=False, recurrent_dropout=0.0, \
-                        kernel_regularizer=keras.regularizers.L1L2(l1=0.01, l2=0.01), recurrent_regularizer=keras.regularizers.L1L2(l1=0.01, l2=0.01))) # , stateful=True
+                        kernel_regularizer=keras.regularizers.L1L2(l1=0.1, l2=0.1),\
+                        recurrent_regularizer=keras.regularizers.L1L2(l1=0.1, l2=0.1))) # , stateful=True
         model.add( LSTM(32, return_sequences=True, kernel_initializer=keras.initializers.Zeros(), \
                         stateful=False, recurrent_dropout=0.0, \
-                        kernel_regularizer=keras.regularizers.L1L2(l1=0.01, l2=0.01), recurrent_regularizer=keras.regularizers.L1L2(l1=0.01, l2=0.01))) # , stateful=True
-        #model.add( Dense(1, activation='relu') ) #
+                        kernel_regularizer=keras.regularizers.L1L2(l1=0.1, l2=0.1),\
+                        recurrent_regularizer=keras.regularizers.L1L2(l1=0.1, l2=0.1))) # , stateful=True
+
         model.add( Dense(16, activation='leaky_relu' ) )  #
         model.add( Dense(units = 1,
                          kernel_initializer = 'random_normal',
