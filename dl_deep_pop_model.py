3e0c6b4c3c3d98ff5f0ef2e3977e2b45e435356d
ivan <imysin@mail.ru>
2024-12-30 22:16:13 +0300

    Working on pop models fitting

--- dl_deep_pop_model.py
+++ dl_deep_pop_model.py
@@ -178,7 +178,7 @@ def fit_dl_model_of_population(datapath, targetpath, logfile):
         # model.add( GRU(16, return_sequences=True, kernel_initializer=keras.initializers.HeUniform(), stateful=True ) ) # , stateful=True
         # model.add( Dense(1, activation='relu') ) #
 
-        model.compile(loss='log_cosh', optimizer=keras.optimizers.Adam(learning_rate=0.001), metrics = ['mae', 'mse', 'mean_squared_logarithmic_error'])
+        model.compile(loss='log_cosh', optimizer=keras.optimizers.Adam(learning_rate=0.0001), metrics = ['mae', 'mse', 'mean_squared_logarithmic_error'])
         #model.compile(loss='mean_squared_logarithmic_error', optimizer='adam', metrics = ['mae',])
 
     if IS_FIT_MODEL:
