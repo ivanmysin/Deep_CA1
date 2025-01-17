5319ee87561a8f0f0f7bf6256b480cefbedec939
ivan <imysin@mail.ru>
2025-01-17 16:47:55 +0300

    Test pop model from L. Chen, S. A. Campbell article

---
+++ tests/izhs_lib.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def izh_simulate(params, eta_params, dt=0.1, duration=200, NN=4000):
+    k = params['k']
+    Vrest =  params['Vrest']
+    VT =  params['Vth']
+    Iext =  params['Iext']
+    Cm =  params['Cm']
+    a =  params['a']
+    b =  params['b']
+    Vpeak = params['Vpeak']
+    Vreset = params['Vmin']
+    d =  params['d']
+
+    V = np.zeros(NN, dtype=float) + params['V0']
+    U = np.zeros(NN, dtype=float) + params['U0']
+
+    eta = eta_params['bar_eta'] + eta_params['Delta_eta'] * np.tan(np.pi*(np.random.rand(NN) - 0.5) )
+    #eta = 1000.1 * eta #* k * Vreset**2
+    print(np.sort(eta))
+    # plt.hist(eta, bins=100)
+    # plt.show()
+
+    Nt = int(duration / dt)
+
+    firings = np.zeros(Nt, dtype=float)
+
+    for ts_idx in range(Nt):
+
+        dVdt = (k * (V - Vrest) * (V - VT) - U + eta + Iext) / Cm
+        dUdt = a * (b * (V - Vrest) - U)
+
+        V = V + dt * dVdt
+        U = U + dt * dUdt
+
+        fired = V > Vpeak
+        V[fired] = Vreset
+        U[fired] += d
+
+        firings[ts_idx] = np.mean(fired)
+
+    return firings
--- tests/test_izh_pop_aprox.py
+++ tests/test_izh_pop_aprox.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+import izhs_lib
 
 def transform_vk(V_k, V_R):
     return 1 + V_k / abs(V_R)
@@ -48,25 +49,32 @@ def transform_tau_s(tau_syn, k1, V_R, C):
     return tau_syn * k1 * abs(V_R) / C
 
 
-def dimensional_to_dimensionless(dimensional_vars, k1, C, V_R):
+def dimensional_to_dimensionless(dimensional_vars):
+    k1 = dimensional_vars['k']
+    C  = dimensional_vars['Cm']
+    V_R = dimensional_vars['Vrest']
+
+
     dimensionless_vars = {}
 
     # Преобразования
-    dimensionless_vars['vk'] = transform_vk(dimensional_vars['Vk'], V_R)
-    dimensionless_vars['wk'] = transform_wk(dimensional_vars['Wk'], k1, V_R)
-    dimensionless_vars['s'] = transform_s(dimensional_vars['s'])
-    dimensionless_vars['T'] = transform_T(dimensional_vars['t'], C, k1, V_R)
-    dimensionless_vars['v_peak'] = transform_v_peak(dimensional_vars['V_peak'], V_R)
-    dimensionless_vars['v_reset'] = transform_v_reset(dimensional_vars['V_reset'], V_R)
-    dimensionless_vars['alpha'] = transform_alpha(dimensional_vars['V_T'], V_R)
-    dimensionless_vars['g_syn'] = transform_g_syn(dimensional_vars['G_syn'], k1, V_R)
-    dimensionless_vars['a'] = transform_a(dimensional_vars['tau_W'], k1, V_R, C)
-    dimensionless_vars['b'] = transform_b(dimensional_vars['beta'], k1, V_R)
-    dimensionless_vars['s_jump'] = transform_s_jump(dimensional_vars['S_jump'], C, k1, V_R)
-    dimensionless_vars['w_jump'] = transform_w_jump(dimensional_vars['W_jump'], k1, V_R)
-    dimensionless_vars['e_r'] = transform_e_r(dimensional_vars['E_r'], V_R)
-    dimensionless_vars['I_ext'] = transform_I(dimensional_vars['I_ext'], k1, V_R)
-    dimensionless_vars['tau_s'] = transform_tau_s(dimensional_vars['tau_syn'], k1, V_R, C)
+    dimensionless_vars['vk'] = transform_vk(dimensional_vars['V0'], V_R)
+    dimensionless_vars['wk'] = transform_wk(dimensional_vars['U0'], k1, V_R)
+    #dimensionless_vars['s'] = transform_s(dimensional_vars['s'])
+    #dimensionless_vars['T'] = transform_T(dimensional_vars['t'], C, k1, V_R)
+    dimensionless_vars['v_peak'] = transform_v_peak(dimensional_vars['Vpeak'], V_R)
+    dimensionless_vars['v_reset'] = transform_v_reset(dimensional_vars['Vmin'], V_R)
+    dimensionless_vars['alpha'] = transform_alpha(dimensional_vars['Vth'], V_R)
+    #dimensionless_vars['g_syn'] = transform_g_syn(dimensional_vars['G_syn'], k1, V_R)
+    dimensionless_vars['a'] = transform_a(1/dimensional_vars['a'], k1, V_R, C)
+    dimensionless_vars['b'] = transform_b(dimensional_vars['b'], k1, V_R)
+    #dimensionless_vars['s_jump'] = transform_s_jump(dimensional_vars['S_jump'], C, k1, V_R)
+    dimensionless_vars['w_jump'] = transform_w_jump(dimensional_vars['d'], k1, V_R)
+
+    #dimensionless_vars['e_r'] = transform_e_r(dimensional_vars['E_r'], V_R)
+
+    dimensionless_vars['I_ext'] = transform_I(dimensional_vars['Iext'], k1, V_R)
+    #dimensionless_vars['tau_s'] = transform_tau_s(dimensional_vars['tau_syn'], k1, V_R, C)
 
     return dimensionless_vars
 
@@ -79,62 +87,92 @@ def mf_izh_ode(y, t, constants):
     :param constants: словарь с постоянными величинами.
     :return: список производных dr/dt, d<v>/dt, d<w>/dt, ds/dt.
     """
-    r, v_avg, w_avg, s = y
+    r, v_avg, w_avg = y # , s
     Delta_eta = constants['Delta_eta']
     alpha = constants['alpha']
     bar_eta = constants['bar_eta']
     I_ext = constants['I_ext']
-    g_syn = constants['g_syn']
-    e_r = constants['e_r']
+    g_syn = 0.0 #constants['g_syn']
+    e_r = 0.0 #constants['e_r']
     a = constants['a']
     b = constants['b']
     w_jump = constants['w_jump']
-    tau_s = constants['tau_s']
-    s_jump = constants['s_jump']
+    # tau_s = constants['tau_s']
+    # s_jump = constants['s_jump']
+    s = 0
 
     dr_dt = Delta_eta / np.pi + 2 * r * v_avg - (alpha + g_syn * s) * r
     dv_avg_dt = v_avg ** 2 - alpha * v_avg - w_avg + bar_eta + I_ext + g_syn * s * (e_r - v_avg) - np.pi ** 2 * r ** 2
     dw_avg_dt = a * (b * v_avg - w_avg) + w_jump * r
-    ds_dt = -s / tau_s + s_jump * r
+    #ds_dt = -s / tau_s + s_jump * r
 
-    return [dr_dt, dv_avg_dt, dw_avg_dt, ds_dt]
+    return [dr_dt, dv_avg_dt, dw_avg_dt] # , ds_dt
+##############################################################
+dim_izh_params = {
+    "V0" : -57.0,
+    "U0" : 0.0,
+
+    "Cm": 114, # * pF,  # /cm**2,
+    "k": 1.19, # * mS / mV,
+    "Vrest": -57.63, # * mV,
+    "Vth": -35.53, #*mV, # np.random.normal(loc=-35.53, scale=4.0, size=NN) * mV,  # -35.53*mV,
+    "Vpeak": 21.72, # * mV,
+    "Vmin": -48.7, # * mV,
+    "a": 0.005, # * ms ** -1,
+    "b": 0.22, # * mS,
+    "d": 2, # * uA,
+
+    "Iext" : 280,
+}
 
 
 # Словарь с константами
-constants = {
+koshi_dencity_params = {
     'Delta_eta': 0.02,
-    'alpha': 0.6215,
-    'bar_eta': 0.25,
-    'I_ext': 0,
-    'g_syn': 1.2308,
-    'e_r': 1,
-    'a': 0.0077,
-    'b': -0.0062,
-    'w_jump': 0.0189,
-    'tau_s': 2.6,
-    's_jump': 1.2308,
+    'bar_eta': 0.0,
 }
 
+# izh_params = {
+#     'alpha': 0.6215,
+#     'I_ext': 0,
+#     'g_syn': 1.2308,
+#     'e_r': 1,
+#     'a': 0.0077,
+#     'b': -0.0062,
+#     'w_jump': 0.0189,
+#     'tau_s': 2.6,
+#     's_jump': 1.2308,
+# }
+
+izh_params = dimensional_to_dimensionless(dim_izh_params)
+
+params = koshi_dencity_params | izh_params
+
 # Начальные условия
-y0 = [0.1, -50, 0, 0]
+y0 = [0.0, izh_params['vk'], izh_params['wk']] # , 0
 
 # Временной интервал
-t = np.linspace(0, 1000, 5000)
+duration = 200
+dt = 0.1
+t = np.linspace(0, duration, int(duration/dt))
 
 # Решение системы ОДУ
-solution = odeint(mf_izh_ode, y0, t, args=(constants,))
+solution = odeint(mf_izh_ode, y0, t, args=(params,))
+
+direct_r = izhs_lib.izh_simulate(dim_izh_params, koshi_dencity_params, dt=dt, duration=duration)
 
 # Извлекаем результаты
 r = solution[:, 0]
 v_avg = solution[:, 1]
 w_avg = solution[:, 2]
-s = solution[:, 3]
+#s = solution[:, 3]
 
-fig, axes = plt.subplots(nrows=4)
+fig, axes = plt.subplots(nrows=3)
 axes[0].plot(t, r)
+axes[0].plot(t, direct_r)
 axes[1].plot(t, v_avg)
 axes[2].plot(t, w_avg)
-axes[3].plot(t, s)
+#axes[3].plot(t, s)
 
 plt.show()
 
