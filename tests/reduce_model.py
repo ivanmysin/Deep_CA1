b5891b1181075dc253af3d2366e9bf939f5a2ca2
ivan <imysin@mail.ru>
2025-01-20 16:21:35 +0300

    Reduce big model

--- tests/reduce_model.py
+++ tests/reduce_model.py
@@ -3,10 +3,10 @@ import os
 os.chdir('../')
 import myconfig
 
-MAX_L = 1700
+MAX_L = 500
 
-neurons_source_path = myconfig.STRUCTURESOFNET + 'neurons.pickle'
-conns_source_path = myconfig.STRUCTURESOFNET + 'connections.pickle'
+neurons_source_path = myconfig.STRUCTURESOFNET + 's_neurons.pickle'
+conns_source_path = myconfig.STRUCTURESOFNET + 's_connections.pickle'
 with open(neurons_source_path, 'rb') as file:
     populations = pickle.load(file)
 with open(conns_source_path, 'rb') as file:
@@ -16,11 +16,13 @@ removed_pops_idxs = []
 old_new_idxs = {}
 new_pops = []
 new_conns = []
+n_types = []
 for pop_idx, pop in enumerate(populations):
     if (pop['y_anat'] > MAX_L) and (not 'generator' in pop['type']):
         removed_pops_idxs.append(pop_idx)
 
     else:
+        n_types.append(pop['type'])
         new_pops.append(pop)
         old_new_idxs[pop_idx] = len(new_pops) - 1
 
@@ -33,9 +35,16 @@ for conn in connections:
         conn['post_idx'] = old_new_idxs[conn['post_idx']]
         new_conns.append(conn)
 
+uniq_types = set(n_types)
 
-neurons_target_path = myconfig.STRUCTURESOFNET + 'test_neurons.pickle'
-conns_target_path = myconfig.STRUCTURESOFNET + 'test_conns.pickle'
+for ut in uniq_types:
+    utc = n_types.count(ut)
+
+    print(ut, utc)
+
+
+neurons_target_path = myconfig.STRUCTURESOFNET + 'neurons.pickle'
+conns_target_path = myconfig.STRUCTURESOFNET + 'connections.pickle'
 with open(neurons_source_path, 'rb') as file:
     populations = pickle.load(file)
 with open(conns_source_path, 'rb') as file:
