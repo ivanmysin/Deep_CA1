03f39389533bcb6b86c38a1c8921b5794fbed44c
ivan <imysin@mail.ru>
2025-04-22 16:39:54 +0300

    Debug mean field model as network

--- run_mean_field_net.py
+++ run_mean_field_net.py
@@ -278,7 +278,7 @@ if __name__ == '__main__':
     #Ys = big_model.predict(Xtrain, batch_size=1)
 
     for y_idx, (key, y) in enumerate(Ytrain.items()):
-        print(np.sum(np.isnan(y) ) )
+        print( y.shape )
         print('##############################')
     #
     #
