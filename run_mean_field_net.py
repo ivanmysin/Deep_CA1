c4c75c89c827f31d985b3a6bb6bdb1e741cb7ab2
ivan <imysin@mail.ru>
2025-04-28 22:53:42 +0300

    Work on mean field layer: add external inputs

--- run_mean_field_net.py
+++ run_mean_field_net.py
@@ -289,9 +289,8 @@ if __name__ == '__main__':
                      save_freq = 2),
     ]
 
-
-
-    history = big_model.fit(Xtrain, Ytrain, epochs=myconfig.EPOCHES_FULL_T, verbose=2, batch_size=1, callbacks=callbacks)
+    with tf.device('/gpu:0'):
+        history = big_model.fit(Xtrain, Ytrain, epochs=myconfig.EPOCHES_FULL_T, verbose=2, batch_size=1, callbacks=callbacks)
 
     # Ys = big_model.predict(Xtrain, batch_size=1)
     #
