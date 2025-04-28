b07415c4733f29c2a535570308f745d884fc3fd1
ivan <imysin@mail.ru>
2025-04-28 23:16:21 +0300

    Work on fit network with mean field layer

--- genloss.py
+++ genloss.py
@@ -497,7 +497,7 @@ class RobastMeanOut(CommonOutProcessing):
     def call(self, simulated_firings):
         selected_firings = tf.boolean_mask(simulated_firings, self.mask, axis=2)
 
-        robast_mean = tf.reduce_mean(selected_firings, axis=1) #exp(tf.reduce_mean(log(selected_firings + 0.0001), axis=1))
+        robast_mean = exp(tf.reduce_mean(log(selected_firings + 0.0001), axis=1))
         robast_mean = tf.reshape(robast_mean, shape=(1, 1, -1))
 
         return robast_mean
