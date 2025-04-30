b26e399eaf550bf48013e33b68a685ef053bb9f6
ivan <imysin@mail.ru>
2025-04-30 16:21:00 +0300

    Debug mean field model as network

--- run_mean_field_net.py
+++ run_mean_field_net.py
@@ -7,7 +7,7 @@ import izhs_lib
 from main import get_dataset
 from pprint import pprint
 import sys
-sys.stdout = open('err.txt', 'w')
+sys.stderr = open('err.txt', 'w')
 
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, RNN, Layer
