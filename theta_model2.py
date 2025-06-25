c4011d6701b98a1ecf09d9d8ccd0b5e959c73ed2
ivan <imysin@mail.ru>
2025-06-25 11:20:12 +0300

    Work on numpy class for mean field integration

--- theta_model2.py
+++ theta_model2.py
@@ -30,8 +30,8 @@ def get_params():
     change_columns = {'Izh Vr': 'Vrest', 'Izh Vt': 'Vth_mean', 'Izh C': 'Cm', 'Izh k': 'k', 'Izh a': 'a', 'Izh b': 'b',
                       'Izh d': 'd', 'Izh Vpeak': 'Vpeak', 'Izh Vmin': 'Vmin'}.values()
 
-    copyed_indx = neurons_params.index[neurons_params['Neuron Type'] == 'CA1 Basket'].value[0]
-    tril_indx = neurons_params.index[neurons_params['Neuron Type'] == 'CA1 Trilaminar'].value[0]
+    copyed_indx = neurons_params.index[neurons_params['Neuron Type'] == 'CA1 Basket'].values[0]
+    tril_indx = neurons_params.index[neurons_params['Neuron Type'] == 'CA1 Trilaminar'].values[0]
 
     for col in change_columns:
         print(neurons_params.loc[copyed_indx, col])
