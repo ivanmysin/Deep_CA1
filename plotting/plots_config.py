148bf9a658f00d935ec70b54d2f8a3232338119b
ivan <imysin@mail.ru>
2025-06-25 13:13:22 +0300

    Work on numpy class for mean field integration

---
+++ plotting/plots_config.py
@@ -0,0 +1,33 @@
+
+plotting_colors = {
+    "neuron_colors" : {
+        "CA1 Pyramidal" : "#FF0000", #(1.0, 0.0, 0.0), # red
+        "CA1 Pyramidal" : "#8B0000", # (0.8, 0.2, 0.0),  #
+
+        "CA1 Basket": (0.0, 0.0, 1.0), # blue
+        "CA1 O-LM": (0.0, 0.0, 0.5), #
+        "CA1 Basket CCK+": (0.0, 1.0, 0.0), # green
+        "CA1 Ivy": (0.0, 0.5, 0.5), #
+        "CA1 Neurogliaform": (0.5, 0.5, 0.5), #
+        "CA1 Bistratified": "#00FFFF", #(0.1, 0.0, 0.5), #
+        "CA1 Axo-Axonic": "#7B68EE", #(1.0, 0.0, 0.5), #
+        "CA1 Perforant Path-Associated": '#32CD32', #(0.0, 1.0, 0.5), #
+
+        "CA1 Interneuron Specific R-O": (0.9, 0.7, 0.0),  #
+        "CA1 Interneuron Specific RO-O": '#FFD700', # (0.0, 0.8, 0.5),  #
+        "CA1 Trilaminar": "#ADD8E6", #(0.0, 0.5, 0.9),  #
+
+        "CA3 Pyramidal": "#FFA07A", #(0.5, 0.5, 0.0), #
+        "EC LIII Pyramidal": "#F08080", # (0.5, 1.0, 0.0), #
+
+
+    },
+
+    "neurons_order" : ["CA1 Pyramidal", "CA1 Pyramidal" ,
+                       "CA1 Basket", "CA1 O-LM", "CA1 Basket CCK+"
+                       "CA1 Ivy", "CA1 Neurogliaform",
+                       "CA1 Bistratified", "CA1 Axo-Axonic",
+                       "CA1 Perforant Path-Associated", "CA1 Interneuron Specific R-O",
+                       "CA1 Interneuron Specific RO-O", "CA1 Trilaminar" ],
+    "generators_order" : ["CA3 Pyramidal", "EC LIII Pyramidal"],
+}
