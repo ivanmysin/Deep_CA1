b1d4530462840dbae799e78df49048cdb770b2c4
ivan <imysin@mail.ru>
2025-07-22 19:10:35 +0300

    Work on R optimization

--- theta_model.py
+++ theta_model.py
@@ -190,7 +190,7 @@ def get_model(params, generators_params, dt, target_params):
 
     big_model.compile(
         optimizer=tf.keras.optimizers.Adam(learning_rate=myconfig.LEARNING_RATE, clipvalue=10.0),
-        loss = lmse_loss, # [lmse_loss, mse_loss],
+        loss = [lmse_loss, lmse_loss],
         loss_weights = [1.0, 1.0],
     )
 
