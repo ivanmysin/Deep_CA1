6339c0fcfec0c36ab3f97ae431ff28bf46b98feb
ivan <imysin@mail.ru>
2025-11-04 22:16:46 +0300

    Work on stable work of the model

--- mean_field_class.py
+++ mean_field_class.py
@@ -288,14 +288,14 @@ class MeanFieldNetwork(Layer):
 
             Isyn += Inmda
 
-        # rates = rates + self.dts_non_dim * (self.Delta_eta / PI + 2 * rates * v_avg - (self.alpha + g_syn_tot) * rates)
+        new_rates = rates + self.dts_non_dim * (self.Delta_eta / PI + 2 * rates * v_avg - (self.alpha + g_syn_tot) * rates)
 
-        new_rates = self.update_rates(v_avg, g_syn_tot, rates)
-        # new_rates = tf.where(rates < 0, 0.0, rates)
+        # new_rates = self.update_rates(v_avg, g_syn_tot, rates)
+        # new_rates = tf.where(new_rates < 0, 0.0, new_rates)
 
         new_v_avg = v_avg + self.dts_non_dim * (v_avg**2 - self.alpha * v_avg - w_avg + self.I_ext + Isyn - (PI*rates)**2)
-        # w_avg = w_avg + self.dts_non_dim * (self.a * (self.b * v_avg - w_avg) + self.w_jump * rates)
-        new_w_avg = self.update_w_avg(w_avg, v_avg, rates)
+        new_w_avg = w_avg + self.dts_non_dim * (self.a * (self.b * v_avg - w_avg) + self.w_jump * rates)
+        # new_w_avg = self.update_w_avg(w_avg, v_avg, rates)
 
         firing_probs = tf.transpose( self.dts_non_dim * rates) #tf.reshape(rates, shape=(-1, 1))
 
