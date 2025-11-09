e76eb321052284a417cfcb3f74fab14267dc6948
ivan <imysin@mail.ru>
2025-11-09 13:45:04 +0300

    Work on stable work of the model

--- variate_theta_freq.py
+++ variate_theta_freq.py
@@ -69,7 +69,8 @@ def make_simulation(params, result_file, simulation_type):
     # npfirings, states = model.predict(generators_firings[:, :start_simulation_idx, :], save_states=False)
     states = model.get_initial_state()
 
-    initial_states = [s[-1] for s in states]
+
+    initial_states = [s for s in states]
 
 
 
