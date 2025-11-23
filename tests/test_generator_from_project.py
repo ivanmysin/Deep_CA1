3b0180e1c6eb8ca5a40fdef6e83d673d1798931a
ivan <imysin@mail.ru>
2025-11-23 16:58:12 +0300

    Work on running full local model

--- tests/test_generator_from_project.py
+++ tests/test_generator_from_project.py
@@ -6,74 +6,55 @@ import pickle
 os.chdir("../")
 from genloss import SpatialThetaGenerators, PhaseLockingOutput
 from pprint import pprint
+import pandas as pd
+
+Van = 20 # cm/sec
+
+sheet_name = 'full_local_model'
+populations = pd.read_excel('./parameters/neurons_parameters.xlsx', sheet_name=sheet_name)
+populations.rename( {'neurons' : 'type'}, axis=1, inplace=True)
+
+populations = populations[populations['Hippocampome_Neurons_Names'] == 'CA1 Pyramidal'][5:10]
+
+populations['SlopePhasePrecession'] = populations['SlopePhasePrecession'] / 360 * 2 * np.pi * Van / 1000
+
+populations['CenterPlaceField'] = populations['CenterPlaceField'] / Van * 1000
+
+
+print(populations['CenterPlaceField'])
+
+populations['ThetaFreq'] = 8.0
+params = populations.to_dict('list')
+
+print(params)
 
 
 
-
-neurons_path = './presimulation_files/neurons.pickle'
-
-
-params = [
-    {
-        "R": 0.25,
-        "OutPlaceFiringRate": 0.5,
-        "OutPlaceThetaPhase": 3.14,
-        "InPlacePeakRate": 8.0,
-        "CenterPlaceField": 5000.0,
-        "SigmaPlaceField": 500,
-        "SlopePhasePrecession": 0.0, # np.deg2rad(10)*10 * 0.001,
-        "PrecessionOnset":  -1.57,
-        "ThetaFreq": 8.0,
-    },
-
-    {
-        "R": 0.5,
-        "OutPlaceFiringRate": 5.5,
-        "OutPlaceThetaPhase": 3.14,
-        "InPlacePeakRate": 18.0,
-        "CenterPlaceField": 5000.0,
-        "SigmaPlaceField": 500,
-        "SlopePhasePrecession": 0.0,  # np.deg2rad(10)*10 * 0.001,
-        "PrecessionOnset": np.nan,  # -1.57,
-        "ThetaFreq": 8.0,
-    },
-]
-
-# with open(neurons_path, "rb") as neurons_file:
-#     populations = pickle.load(neurons_file)
-# populations[113]['PrecessionOnset'] = np.nan
-# pprint( populations[113])
-#
-# params = [populations[113], ]
-# for pop in populations:
-#     if "_generator" in pop['type']:
-#         params.append(pop)
-#
-#         print(pop['CenterPlaceField'])
-dt = 0.01
+dt = 0.5
+duration = 15000
 genrators = SpatialThetaGenerators(params)
-t = tf.range(0, 125, dt, dtype=tf.float32)
+t = tf.range(0, duration, dt, dtype=tf.float32)
 t = tf.reshape(t, shape=(1, -1, 1))
 
 firings = genrators(t)
 print(tf.shape(firings))
 
+#
+# MeanFirings = [p["OutPlaceFiringRate"] for p in params]
+# Rtar = [p["R"] for p in params]
+#
+# # MeanFirings, ThetaFreq=5.0, dt=0.1
+# modulation_layer = PhaseLockingOutput(MeanFirings, ThetaFreq=params[0]['ThetaFreq'], dt=dt)
+#
+# Rsim = modulation_layer(firings)
+#
+# print(Rtar)
+# print(Rsim.numpy().ravel())
+#
+#
+# df = firings[0, :-1, 0] - firings[0, 1:, 0]
 
-MeanFirings = [p["OutPlaceFiringRate"] for p in params]
-Rtar = [p["R"] for p in params]
-
-# MeanFirings, ThetaFreq=5.0, dt=0.1
-modulation_layer = PhaseLockingOutput(MeanFirings, ThetaFreq=params[0]['ThetaFreq'], dt=dt)
-
-Rsim = modulation_layer(firings)
-
-print(Rtar)
-print(Rsim.numpy().ravel())
-
-
-df = firings[0, :-1, 0] - firings[0, 1:, 0]
-
-fig, axes = plt.subplots(nrows=2)
-axes[0].plot(t[0, :, 0], firings[0, :, 0])
-axes[1].plot(t[0, 1:, 0],df)
+fig, axes = plt.subplots(nrows=1)
+axes.plot(t[0, :, 0], firings[0, :, :10])
+# axes[1].plot(t[0, 1:, 0],df)
 plt.show()
--- theta_model2.py
+++ theta_model2.py
@@ -20,20 +20,21 @@ import myconfig
 from myutils import get_net_params, get_gen_params
 
 def get_base_params(path_to_base_model, sheet_name):
-    base_params = get_net_params(SOURCE_MODEL)
+    base_params = get_net_params(path_to_base_model)
 
-    populations = pd.read_excel('./parameters/neurons_parameters.xlsx', sheet_name='local_model')
+
+    populations = pd.read_excel('./parameters/neurons_parameters.xlsx', sheet_name=sheet_name)
     populations.rename( {'neurons' : 'type'}, axis=1, inplace=True)
     populations = populations[populations['Npops'] > 0]
 
-    pop_types = populations['Hippocampome_Neurons_Names'].to_list()
+    pop_types = populations['Model_Neurons_Names'].to_list()
     pconn_mask = base_params['pconn'] == 0
     Ngenerators = sum(populations['Simulated_Type'] == 'generator')
 
     simulated_types = pop_types[:-Ngenerators]
     optim_parameters_neurons = pd.DataFrame( {'Neuron Type' : simulated_types, 'I_ext': base_params['I_ext'] } )
 
-    optim_parameters_connection =  pd.DataFrame(columns=['Presynaptic Neuron Type', 'Postsynaptic Neuron Type', 'gsyn_max', 'tau_d', 'tau_r', 'tau_f', 'Uinc'])
+    optim_parameters_connection =  pd.DataFrame(columns=['Presynaptic Neuron Type', 'Postsynaptic Neuron Type', 'gsyn_max', 'tau_d', 'tau_r', 'tau_f', 'Uinc', 'Erev'])
 
     for pre_idx, presynaptic_type in enumerate(pop_types):
         for post_idx, postsynaptic_type in enumerate(pop_types[:-Ngenerators]):
@@ -50,20 +51,143 @@ def get_base_params(path_to_base_model, sheet_name):
                 'tau_r': base_params['tau_r'][pre_idx, post_idx],
                 'tau_f': base_params['tau_f'][pre_idx, post_idx],
                 'Uinc': base_params['Uinc'][pre_idx, post_idx],
+                'Erev': base_params['e_r'][pre_idx, post_idx],
             }
 
+
+
             optim_parameters_connection.loc[len(optim_parameters_connection)] = p
+
+
 
 
     return optim_parameters_connection, optim_parameters_neurons
 
 
-def get_params():
+def get_params(optim_parameters_connection, optim_parameters_neurons):
+    neurons_params = pd.read_csv(myconfig.IZHIKEVICNNEURONSPARAMS)
+    neurons_params.rename(
+        {'Izh Vr': 'Vrest', 'Izh Vt': 'Vth_mean', 'Izh C': 'Cm', 'Izh k': 'k', 'Izh a': 'a', 'Izh b': 'b', 'Izh d': 'd',
+         'Izh Vpeak': 'Vpeak', 'Izh Vmin': 'Vmin'}, axis=1, inplace=True)
+
+    synapses_params = optim_parameters_connection
+    synapses_params.rename({"g": "gsyn_max", "u": "Uinc", "Connection Probability": "pconn"}, axis=1, inplace=True)
+
+
+    populations = pd.read_excel(myconfig.FIRINGSNEURONPARAMS, sheet_name='local_model')
+    populations = populations[populations['Npops'] > 0]
+
+    params = {
+        'I_ext' : [],
+    }
+    dimpopparams = {
+        'dt_dim' : myconfig.DT,
+        'Delta_eta' : [],
+    }
+
+    generators_params = []
+
+    for pop_idx, pop in populations.iterrows():
+
+        if pop['Simulated_Type'] == 'generator':
+            generators_params.append(pop.to_dict())
+            continue
+
+        hippocampome_pop_type = pop['Hippocampome_Neurons_Names']
 
 
 
+        p = neurons_params[neurons_params["Neuron Type"] == hippocampome_pop_type]
 
-    return params, generators_params, target_params
+        try:
+            Iext_pop = optim_parameters_neurons['I_ext'][optim_parameters_neurons['Neuron Type'] == hippocampome_pop_type][0]
+            params['I_ext'].append(Iext_pop)
+        except KeyError:
+            params['I_ext'].append(0.0)
+
+        try:
+            dimpopparams['Delta_eta'].append(pop['Delta_eta'])
+        except KeyError:
+            dimpopparams['Delta_eta'].append(myconfig.DELTA_ETA)
+
+        for key in p:
+            val = p[key].values[0]
+            try:
+                val = float(val)
+            except ValueError:
+                continue
+
+            if key in dimpopparams.keys():
+                dimpopparams[key].append(val)
+            else:
+                dimpopparams[key] = [val, ]
+
+
+    for key, val in dimpopparams.items():
+        dimpopparams[key] = np.asarray(val)
+
+    NN = len(populations)
+    Nsim = NN - len(generators_params)
+    gsyn_max = np.zeros(shape=(NN, Nsim), dtype=np.float32)
+
+
+
+    params['gsyn_max'] = gsyn_max
+    params["Erev"] = np.zeros_like(gsyn_max)
+
+    params['pconn'] = np.zeros_like(gsyn_max)
+    params['tau_d'] = np.zeros_like(gsyn_max) + 100  # + tau_d
+    params['tau_r'] = np.zeros_like(gsyn_max) + 10  # + tau_r
+    params['tau_f'] = np.zeros_like(gsyn_max) + 5  # + tau_f
+    params['Uinc'] = np.zeros_like(gsyn_max) + 0.5  # + Uinc
+
+    for pre_idx, (_, pre_pop) in enumerate(populations.iterrows()):
+        for post_idx, (_, post_pop) in enumerate(populations.iterrows()):
+            if post_pop['Simulated_Type'] == 'generator':
+                continue
+
+            pre_type = pre_pop['Model_Neurons_Names']
+            post_type = post_pop['Model_Neurons_Names']
+
+            syn = synapses_params[(synapses_params['Presynaptic Neuron Type'] == pre_type) & (
+                synapses_params['Postsynaptic Neuron Type'] == post_type)]
+
+            if len(syn) == 0:
+                continue
+
+            params['pconn'][pre_idx, post_idx] = 1 # syn['pconn'].values[0] !!!!!!!!! тут добавить связи между пирамидами!!!!
+            Uinc = syn['Uinc'].values[0]
+            tau_r = syn['tau_r'].values[0]
+            tau_f = syn['tau_f'].values[0]
+            tau_d = syn['tau_d'].values[0]
+
+            gsyn = syn['gsyn_max'].values[0]
+            Erev = syn['Erev'].values[0]
+
+            params['Uinc'][pre_idx, post_idx] = Uinc
+            params['tau_r'][pre_idx, post_idx] = tau_r
+            params['tau_f'][pre_idx, post_idx] = tau_f
+            params['tau_d'][pre_idx, post_idx] = tau_d
+
+            params['gsyn_max'][pre_idx, post_idx] = gsyn
+            params['Erev'][pre_idx, post_idx] = Erev
+
+    params_dimless = izhs_lib.dimensional_to_dimensionless_all(dimpopparams)
+
+    params = params | params_dimless
+
+
+    for p in generators_params:
+        p['ThetaFreq'] = myconfig.ThetaFreq
+
+    populations['ThetaFreq'] = myconfig.ThetaFreq
+
+    target_params = populations[ (populations['Simulated_Type'] == 'simulated')  ]
+
+
+    output_masks = []
+
+    return params, generators_params, target_params, output_masks
 ########################################################################
 def get_model(params, generators_params, dt):
     input = Input(shape=(None, 1), batch_size=1)
@@ -109,9 +233,9 @@ nbatches = 20
 
 optim_parameters_connection, optim_parameters_neurons = get_base_params(SOURCE_MODEL, 'local_model')
 
-print(optim_parameters_neurons)
 
-params, generators_params, target_params = get_params(optim_parameters_connection)
+
+params, generators_params, target_params, output_masks = get_params(optim_parameters_connection, optim_parameters_neurons)
 
 '''
 Xtrain, Ytrain = get_dataset(target_params, myconfig.DT, batch_len, nbatches)
