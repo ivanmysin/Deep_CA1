1532f1a8c10b5bbfe005b6d9df942d413a33d7e7
ivan <imysin@mail.ru>
2025-12-11 22:43:27 +0300

    Work on running full local model. Fit Delta eta in min max range.

--- theta_model2.py
+++ theta_model2.py
@@ -285,7 +285,7 @@ def get_model(params, generators_params, dt, output_masks):
 
     big_model.compile(
         optimizer=tf.keras.optimizers.Adam(learning_rate=myconfig.LEARNING_RATE, clipvalue=10.0),
-        loss = MultiLoss(), # tf.keras.losses.MeanSquaredLogarithmicError() # loss_funcs,
+        loss = tf.keras.losses.MeanSquaredLogarithmicError() # loss_funcs, # MultiLoss(), #
         # loss_weights = {
         #     "full_output" : 1.0,
         #     "phase_output": 0.25,
